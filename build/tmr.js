1d7540ac61c5f8494d4b636ac3bd2709467eb375
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-02 16:15:43 -0700

    add node-sass; set dev server to dev

--- build/tmr.js
+++ build/tmr.js
@@ -6,6 +6,7 @@ async function setSignals() {
   const strength_5g_el = document.getElementById("5g-strength");
   const statsEl = document.createElement("script");
   statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
+  statsEl.setAttribute("crossorigin", "true");
   document.body.appendChild(statsEl);
   statsEl.addEventListener("load", (e) => console.log(e));
 }
--- src/tmr.ts
+++ src/tmr.ts
@@ -10,8 +10,24 @@ async function setSignals(): Promise<void> {
   const statsEl = document.createElement("script");
   statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
   //  statsEl.type = 'application/json';
+  statsEl.setAttribute('crossorigin', 'true');
   document.body.appendChild(statsEl);
   statsEl.addEventListener('load', e => console.log(e));
+
+  const stats = {
+    '4g': {
+      bars: 0
+    },
+    '5g': {
+      bars: 0
+    }
+  };
+  stats['5g'].bars = 4;
+  stats['4g'].bars = 3;
+
+  strength_4g_el.style.width = stats['4g'].bars / 5 * 100 + '%';
+  strength_5g_el.style.width = stats['5g'].bars / 5 * 100 + '%';
+
 }
 
 main();
