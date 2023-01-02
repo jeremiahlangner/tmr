4b41a6929889e2b44c56a737e52f7ea73c83ddfb
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-02 10:57:09 -0700

    remove inappropriate html

--- build/tmr.js
+++ build/tmr.js
@@ -4,7 +4,10 @@ function main() {
 async function setSignals() {
   const strength_4g_el = document.getElementById("4g-strength");
   const strength_5g_el = document.getElementById("5g-strength");
-  const stats = fetch("http://192.168.12.1/TMI/v1/gateway?get=all").then((res) => res.json());
-  console.log(stats);
+  const statsEl = document.createElement("script");
+  statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
+  statsEl.id = "stats";
+  statsEl.type = "application/json";
+  document.body.appendChild(statsEl);
 }
 main();
--- src/tmr.ts
+++ src/tmr.ts
@@ -6,8 +6,12 @@ async function setSignals(): Promise<void> {
   const strength_4g_el = document.getElementById('4g-strength');
   const strength_5g_el = document.getElementById('5g-strength');
 
-  const stats = fetch('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.json());
-  console.log(stats);
+  // const stats = fetch('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.json());
+  const statsEl = document.createElement("script");
+  statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
+  statsEl.id = 'stats';
+  statsEl.type = 'application/json';
+  document.body.appendChild(statsEl);
 }
 
 main();
