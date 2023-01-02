337d26137ff53bd49bcedb7b2e66e755c42c988f
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-02 10:40:21 -0700

    fix local query path for stats

--- build/tmr.js
+++ build/tmr.js
@@ -4,7 +4,7 @@ function main() {
 async function setSignals() {
   const strength_4g_el = document.getElementById("4g-strength");
   const strength_5g_el = document.getElementById("5g-strength");
-  const stats = fetch("http://192.168.12.1/gateway?get=all").then((res) => res.json());
+  const stats = fetch("http://192.168.12.1/TMI/v1/gateway?get=all").then((res) => res.json());
   console.log(stats);
 }
 main();
--- src/tmr.ts
+++ src/tmr.ts
@@ -6,7 +6,7 @@ async function setSignals(): Promise<void> {
   const strength_4g_el = document.getElementById('4g-strength');
   const strength_5g_el = document.getElementById('5g-strength');
 
-  const stats = fetch('http://192.168.12.1/gateway?get=all').then(res => res.json());
+  const stats = fetch('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.json());
   console.log(stats);
 }
 
