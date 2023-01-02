e615b1951272baec6b20be1c472252a64d6d0416
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-02 16:44:47 -0700

    add basic file service

--- server.ts
+++ server.ts
@@ -1,9 +1,19 @@
 import Fastify from 'fastify';
+import { readdirSync, readFileSync } from 'fs';
+
 const fastify = Fastify({
   logger: true
 });
 
 async function main(): Promise<void> {
+  const files = {};
+  const list = readdirSync('./build/');
+
+  list.forEach(f => {
+    files[f] = readFileSync('./build/' + f, { encoding: 'utf8' });
+    fastify.get('/' + f, async (request, reply) => files[f]);
+  });
+
   try {
     await fastify.listen({
       port: 3000
