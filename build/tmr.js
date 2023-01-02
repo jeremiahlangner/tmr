f057e77ca59cbc6def9d827ec6972309874998e2
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-02 11:14:17 -0700

    begin refactor to use worker for cors

--- build/tmr.js
+++ build/tmr.js
@@ -6,8 +6,7 @@ async function setSignals() {
   const strength_5g_el = document.getElementById("5g-strength");
   const statsEl = document.createElement("script");
   statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
-  statsEl.id = "stats";
-  statsEl.type = "application/json";
   document.body.appendChild(statsEl);
+  statsEl.addEventListener("load", (e) => console.log(e));
 }
 main();
--- src/tmr.ts
+++ src/tmr.ts
@@ -9,9 +9,9 @@ async function setSignals(): Promise<void> {
   // const stats = fetch('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.json());
   const statsEl = document.createElement("script");
   statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
-  statsEl.id = 'stats';
-  statsEl.type = 'application/json';
+  //  statsEl.type = 'application/json';
   document.body.appendChild(statsEl);
+  statsEl.addEventListener('load', e => console.log(e));
 }
 
 main();
