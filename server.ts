41b0b2156a53b34b04e759b06ab3eb4ad911e87a
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-03 20:47:41 -0700

    add / route; add axios

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import axios from 'axios';
 import { readdirSync, readFileSync } from 'fs';
 
 const fastify = Fastify({
@@ -9,10 +10,16 @@ async function main(): Promise<void> {
   const files = {};
   const list = readdirSync('./build/');
 
+  // fast routes
   list.forEach(f => {
     files[f] = readFileSync('./build/' + f, { encoding: 'utf8' });
-    fastify.get('/' + f, async (request, reply) => files[f]);
+    fastify.get('/' + f, async () => files[f]);
   });
+  fastify.get('/', async () => readFileSync('./build/index.html', { encoding: 'utf8' }));
+  fastify.get('/stats', async () => {
+    return await axios.get('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.data);
+  });
+
 
   try {
     await fastify.listen({
