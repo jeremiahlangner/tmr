add2cec35dddf995772f3fbde41c2071f141c338
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-04 19:19:35 -0700

    adjust styles

--- src/tmr.ts
+++ src/tmr.ts
@@ -6,27 +6,11 @@ async function setSignals(): Promise<void> {
   const strength_4g_el = document.getElementById('4g-strength');
   const strength_5g_el = document.getElementById('5g-strength');
 
-  // const stats = fetch('http://192.168.12.1/TMI/v1/gateway?get=all').then(res => res.json());
-  const statsEl = document.createElement("script");
-  statsEl.setAttribute("src", "http://192.168.12.1/TMI/v1/gateway?get=all");
-  //  statsEl.type = 'application/json';
-  statsEl.setAttribute('crossorigin', 'true');
-  document.body.appendChild(statsEl);
-  statsEl.addEventListener('load', e => console.log(e));
+  const stats = await fetch('http://localhost:3000/stats').then(res => res.json());
+  console.log(stats);
 
-  const stats = {
-    '4g': {
-      bars: 0
-    },
-    '5g': {
-      bars: 0
-    }
-  };
-  stats['5g'].bars = 4;
-  stats['4g'].bars = 3;
-
-  strength_4g_el.style.width = stats['4g'].bars / 5 * 100 + '%';
-  strength_5g_el.style.width = stats['5g'].bars / 5 * 100 + '%';
+  strength_4g_el!.style.width = stats.signal['4g'].bars / 5 * 100 + '%';
+  strength_5g_el!.style.width = stats.signal['5g'].bars / 5 * 100 + '%';
 
 }
 
