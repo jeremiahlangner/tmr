4af67ef0eace19962a241ba702ee6cba961a6ecc
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-05 20:29:01 -0700

    display text rep of signal stats

--- build/tmr.js
+++ build/tmr.js
@@ -12,5 +12,31 @@ function render() {
   const strength_5g_el = document.getElementById("5g-strength");
   strength_4g_el.style.width = stats.signal["4g"].bars / 5 * 100 + "%";
   strength_5g_el.style.width = stats.signal["5g"].bars / 5 * 100 + "%";
+  const _4gStats = document.getElementById("4g-stats");
+  _4gStats.innerHTML = "";
+  for (const key in stats.signal["4g"]) {
+    const item = document.createElement("div");
+    const label = document.createElement("span");
+    label.classList.add("stat-label");
+    label.innerText = key;
+    const stat = document.createElement("span");
+    stat.innerText = stats.signal["4g"][key];
+    item.appendChild(label);
+    item.appendChild(stat);
+    _4gStats.appendChild(item);
+  }
+  const _5gStats = document.getElementById("5g-stats");
+  _5gStats.innerHTML = "";
+  for (const key in stats.signal["5g"]) {
+    const item = document.createElement("div");
+    const label = document.createElement("span");
+    label.classList.add("stat-label");
+    label.innerText = key;
+    const stat = document.createElement("span");
+    stat.innerText = stats.signal["5g"][key];
+    item.appendChild(label);
+    item.appendChild(stat);
+    _5gStats.appendChild(item);
+  }
 }
 main();
