c512093abd9c3cb2ce156dd6a46464277580e1c9
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-05 19:53:17 -0700

    add interval function

--- build/tmr.js
+++ build/tmr.js
@@ -1,11 +1,15 @@
-function main() {
-  setSignals();
+let stats;
+async function main() {
+  setInterval(refreshStats, 1e3);
 }
-async function setSignals() {
+async function refreshStats() {
+  stats = await fetch("http://localhost:3000/stats").then((res) => res.json());
+  console.log(stats);
+  render();
+}
+function render() {
   const strength_4g_el = document.getElementById("4g-strength");
   const strength_5g_el = document.getElementById("5g-strength");
-  const stats = await fetch("http://localhost:3000/stats").then((res) => res.json());
-  console.log(stats);
   strength_4g_el.style.width = stats.signal["4g"].bars / 5 * 100 + "%";
   strength_5g_el.style.width = stats.signal["5g"].bars / 5 * 100 + "%";
 }
--- src/tmr.ts
+++ src/tmr.ts
@@ -1,17 +1,19 @@
-function main(): void {
-  setSignals();
+let stats;
+
+async function main(): Promise<void> {
+  setInterval(refreshStats, 1000);
 }
 
-async function setSignals(): Promise<void> {
+async function refreshStats() {
+  stats = await fetch('http://localhost:3000/stats').then(res => res.json());
+  console.log(stats);
+  render();
+}
+
+function render() {
   const strength_4g_el = document.getElementById('4g-strength');
   const strength_5g_el = document.getElementById('5g-strength');
-
-  const stats = await fetch('http://localhost:3000/stats').then(res => res.json());
-  console.log(stats);
-
   strength_4g_el!.style.width = stats.signal['4g'].bars / 5 * 100 + '%';
   strength_5g_el!.style.width = stats.signal['5g'].bars / 5 * 100 + '%';
-
 }
-
 main();
