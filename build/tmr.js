5ebc19cb24f3217fc2727bbb9842294985c5a93b
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-06 19:24:05 -0700

    fix export error, since not compiling modules atm

--- build/tmr.js
+++ build/tmr.js
@@ -1,64 +1,61 @@
-let stats;
-async function main() {
-  setInterval(refreshStats, 500);
-}
-export class RouterDash {
-  constructor() {
+class RouterDash {
+  constructor(interval) {
+    this._stats = {};
     this._4gStats = {};
     this._5gStats = {};
     this._deviceDetails = {};
+    this._updateInterval = setInterval(this.refreshStats, interval);
+  }
+  async refreshStats() {
+    this._stats = await fetch("http://localhost:3000/stats").then((res) => res.json());
+    this.render();
+  }
+  render() {
+    const strength_4g_el = document.getElementById("4g-strength");
+    const strength_5g_el = document.getElementById("5g-strength");
+    strength_4g_el.style.width = this._stats.signal["4g"].bars / 5 * 100 + "%";
+    strength_5g_el.style.width = this._stats.signal["5g"].bars / 5 * 100 + "%";
+    const _4gStatsEl = document.getElementById("4g-stats");
+    _4gStatsEl.innerHTML = "";
+    for (const key in this._stats.signal["4g"]) {
+      const item = document.createElement("div");
+      const label = document.createElement("span");
+      label.classList.add("stat-label");
+      label.innerText = key;
+      const stat = document.createElement("span");
+      stat.innerText = this._stats.signal["4g"][key];
+      item.appendChild(label);
+      item.append(": ");
+      item.appendChild(stat);
+      _4gStatsEl.appendChild(item);
+    }
+    const _5gStatsEl = document.getElementById("5g-stats");
+    _5gStatsEl.innerHTML = "";
+    for (const key in this._stats.signal["5g"]) {
+      const item = document.createElement("div");
+      const label = document.createElement("span");
+      label.classList.add("stat-label");
+      label.innerText = key;
+      const stat = document.createElement("span");
+      stat.innerText = this._stats.signal["5g"][key];
+      item.appendChild(label);
+      item.append(": ");
+      item.appendChild(stat);
+      _5gStatsEl.appendChild(item);
+    }
+    const deviceDetails = document.getElementById("device-details");
+    for (const key in this._stats.device) {
+      const item = document.createElement("div");
+      const label = document.createElement("span");
+      label.classList.add("stat-label");
+      label.innerText = key;
+      const stat = document.createElement("span");
+      stat.innerText = this._stats.device[key];
+      item.appendChild(label);
+      item.append(": ");
+      item.appendChild(stat);
+      deviceDetails.appendChild(item);
+    }
   }
 }
-async function refreshStats() {
-  stats = await fetch("http://localhost:3000/stats").then((res) => res.json());
-  console.log(stats);
-  render();
-}
-function render() {
-  const strength_4g_el = document.getElementById("4g-strength");
-  const strength_5g_el = document.getElementById("5g-strength");
-  strength_4g_el.style.width = stats.signal["4g"].bars / 5 * 100 + "%";
-  strength_5g_el.style.width = stats.signal["5g"].bars / 5 * 100 + "%";
-  const _4gStats = document.getElementById("4g-stats");
-  _4gStats.innerHTML = "";
-  for (const key in stats.signal["4g"]) {
-    const item = document.createElement("div");
-    const label = document.createElement("span");
-    label.classList.add("stat-label");
-    label.innerText = key;
-    const stat = document.createElement("span");
-    stat.innerText = stats.signal["4g"][key];
-    item.appendChild(label);
-    item.append(": ");
-    item.appendChild(stat);
-    _4gStats.appendChild(item);
-  }
-  const _5gStats = document.getElementById("5g-stats");
-  _5gStats.innerHTML = "";
-  for (const key in stats.signal["5g"]) {
-    const item = document.createElement("div");
-    const label = document.createElement("span");
-    label.classList.add("stat-label");
-    label.innerText = key;
-    const stat = document.createElement("span");
-    stat.innerText = stats.signal["5g"][key];
-    item.appendChild(label);
-    item.append(": ");
-    item.appendChild(stat);
-    _5gStats.appendChild(item);
-  }
-  const deviceDetails = document.getElementById("device-details");
-  for (const key in stats.device) {
-    const item = document.createElement("div");
-    const label = document.createElement("span");
-    label.classList.add("stat-label");
-    label.innerText = key;
-    const stat = document.createElement("span");
-    stat.innerText = stats.device[key];
-    item.appendChild(label);
-    item.append(": ");
-    item.appendChild(stat);
-    deviceDetails.appendChild(item);
-  }
-}
-main();
+const dash = new RouterDash(500);
--- src/tmr.ts
+++ src/tmr.ts
@@ -1,73 +1,77 @@
-let stats;
-
-async function main(): Promise<void> {
-  setInterval(refreshStats, 500);
+interface Gateway {
+  device: any,
+  signal: any,
 }
 
-export class RouterDash {
+class RouterDash {
+  _updateInterval;
+  _stats: Partial<Gateway> = {};
   _4gStats = {};
   _5gStats = {};
   _deviceDetails = {};
 
-  constructor() { }
-
-}
-
-async function refreshStats() {
-  stats = await fetch('http://localhost:3000/stats').then(res => res.json());
-  console.log(stats);
-  render();
-}
-
-function render() {
-  const strength_4g_el = document.getElementById('4g-strength');
-  const strength_5g_el = document.getElementById('5g-strength');
-  strength_4g_el!.style.width = stats.signal['4g'].bars / 5 * 100 + '%'; // eslint-disable-line
-  strength_5g_el!.style.width = stats.signal['5g'].bars / 5 * 100 + '%'; // eslint-disable-line
-
-  const _4gStats = document.getElementById('4g-stats');
-  _4gStats!.innerHTML = ''; // eslint-disable-line
-  for (const key in stats.signal['4g']) {
-    const item = document.createElement('div');
-    const label = document.createElement('span');
-    label.classList.add('stat-label');
-    label.innerText = key;
-    const stat = document.createElement('span');
-    stat.innerText = stats.signal['4g'][key];
-    item.appendChild(label);
-    item.append(': ');
-    item.appendChild(stat);
-    _4gStats!.appendChild(item); // eslint-disable-line
-
+  constructor(interval: number) {
+    this._updateInterval = setInterval(this.refreshStats, interval);
   }
 
-  const _5gStats = document.getElementById('5g-stats');
-  _5gStats!.innerHTML = ''; // eslint-disable-line
-  for (const key in stats.signal['5g']) {
-    const item = document.createElement('div');
-    const label = document.createElement('span');
-    label.classList.add('stat-label');
-    label.innerText = key;
-    const stat = document.createElement('span');
-    stat.innerText = stats.signal['5g'][key];
-    item.appendChild(label);
-    item.append(': ');
-    item.appendChild(stat);
-    _5gStats!.appendChild(item); // eslint-disable-line
+  async refreshStats() {
+    this._stats = await fetch('http://localhost:3000/stats').then(res => res.json());
+    this.render();
   }
 
-  const deviceDetails = document.getElementById('device-details');
-  for (const key in stats.device) {
-    const item = document.createElement('div');
-    const label = document.createElement('span');
-    label.classList.add('stat-label');
-    label.innerText = key;
-    const stat = document.createElement('span');
-    stat.innerText = stats.device[key];
-    item.appendChild(label);
-    item.append(': ');
-    item.appendChild(stat);
-    deviceDetails!.appendChild(item); // eslint-disable-line
+  render() {
+    const strength_4g_el = document.getElementById('4g-strength');
+    const strength_5g_el = document.getElementById('5g-strength');
+    strength_4g_el!.style.width = this._stats.signal['4g'].bars / 5 * 100 + '%'; // eslint-disable-line
+    strength_5g_el!.style.width = this._stats.signal['5g'].bars / 5 * 100 + '%'; // eslint-disable-line
+
+    // Convention to start var with a number?
+    const _4gStatsEl = document.getElementById('4g-stats');
+    _4gStatsEl!.innerHTML = ''; // eslint-disable-line
+    for (const key in this._stats.signal['4g']) {
+      const item = document.createElement('div');
+      const label = document.createElement('span');
+      label.classList.add('stat-label');
+      label.innerText = key;
+      const stat = document.createElement('span');
+      stat.innerText = this._stats.signal['4g'][key];
+      item.appendChild(label);
+      item.append(': ');
+      item.appendChild(stat);
+      _4gStatsEl!.appendChild(item); // eslint-disable-line
+
+    }
+
+    // Convention to start var with a number?
+    const _5gStatsEl = document.getElementById('5g-stats');
+    _5gStatsEl!.innerHTML = ''; // eslint-disable-line
+    for (const key in this._stats.signal['5g']) {
+      const item = document.createElement('div');
+      const label = document.createElement('span');
+      label.classList.add('stat-label');
+      label.innerText = key;
+      const stat = document.createElement('span');
+      stat.innerText = this._stats.signal['5g'][key];
+      item.appendChild(label);
+      item.append(': ');
+      item.appendChild(stat);
+      _5gStatsEl!.appendChild(item); // eslint-disable-line
+    }
+
+    const deviceDetails = document.getElementById('device-details');
+    for (const key in this._stats.device) {
+      const item = document.createElement('div');
+      const label = document.createElement('span');
+      label.classList.add('stat-label');
+      label.innerText = key;
+      const stat = document.createElement('span');
+      stat.innerText = this._stats.device[key];
+      item.appendChild(label);
+      item.append(': ');
+      item.appendChild(stat);
+      deviceDetails!.appendChild(item); // eslint-disable-line
+    }
   }
 }
-main();
+
+const dash = new RouterDash(500);
