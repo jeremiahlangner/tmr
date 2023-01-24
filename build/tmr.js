a9a6f3f42b60b6d17c6827c7e71b9a04d52c86c0
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-23 20:06:11 -0700

    consolidate signal updates

--- build/tmr.js
+++ build/tmr.js
@@ -75,8 +75,7 @@ class RouterDash {
     const strength_5g_el = document.getElementById("5g-strength");
     strength_4g_el.style.width = this._stats.signal["4g"].bars / 5 * 100 + "%";
     strength_5g_el.style.width = this._stats.signal["5g"].bars / 5 * 100 + "%";
-    this.update4g();
-    this.update5g();
+    this.updateSignals();
     this.updateDeviceDetails();
   }
   updateDeviceDetails() {
@@ -98,43 +97,25 @@ class RouterDash {
     }
     this._deviceDetails = this._stats.device;
   }
-  update4g() {
-    if (this._4gStats == this._stats.signal["4g"])
-      return;
-    const _4gStatsEl = document.getElementById("4g-stats");
-    _4gStatsEl.innerHTML = "";
-    for (const key in this._stats.signal["4g"]) {
-      const item = document.createElement("div");
-      const label = document.createElement("span");
-      label.classList.add("stat-label");
-      label.innerText = key;
-      const stat = document.createElement("span");
-      stat.innerText = this._stats.signal["4g"][key];
-      item.appendChild(label);
-      item.append(": ");
-      item.appendChild(stat);
-      _4gStatsEl.appendChild(item);
+  updateSignals() {
+    for (const signal in this._stats.signal) {
+      if (this["_" + signal + "Stats"] == this._stats.signal[signal])
+        return;
+      const el = document.getElementById(signal + "-stats");
+      for (const key in this._stats.signal[signal]) {
+        const item = document.createElement("div");
+        const label = document.createElement("span");
+        label.classList.add("stat-label");
+        label.innerText = key;
+        const stat = document.createElement("span");
+        stat.innerText = this._stats.signal[signal][key];
+        item.appendChild(label);
+        item.append(": ");
+        item.appendChild(stat);
+        el.appendChild(item);
+      }
+      this["_" + signal + "Stats"] = this._stats.signal["4g"];
     }
-    this._4gStats = this._stats.signal["4g"];
-  }
-  update5g() {
-    if (this._5gStats == this._stats.signal["5g"])
-      return;
-    const _5gStatsEl = document.getElementById("5g-stats");
-    _5gStatsEl.innerHTML = "";
-    for (const key in this._stats.signal["5g"]) {
-      const item = document.createElement("div");
-      const label = document.createElement("span");
-      label.classList.add("stat-label");
-      label.innerText = key;
-      const stat = document.createElement("span");
-      stat.innerText = this._stats.signal["5g"][key];
-      item.appendChild(label);
-      item.append(": ");
-      item.appendChild(stat);
-      _5gStatsEl.appendChild(item);
-    }
-    this._5gStats = this._stats.signal["5g"];
   }
   async getConfiguration() {
     this._configuration = await fetch("http://localhost:3000/configuration", {
--- src/tmr.ts
+++ src/tmr.ts
@@ -88,8 +88,7 @@ class RouterDash {
     strength_4g_el!.style.width = this._stats.signal['4g'].bars / 5 * 100 + '%'; // eslint-disable-line
     strength_5g_el!.style.width = this._stats.signal['5g'].bars / 5 * 100 + '%'; // eslint-disable-line
 
-    this.update4g();
-    this.update5g();
+    this.updateSignals();
     this.updateDeviceDetails();
   }
 
@@ -112,45 +111,24 @@ class RouterDash {
     this._deviceDetails = this._stats.device;
   }
 
-  update4g() {
-    // Convention to start var with a number?
-    if (this._4gStats == this._stats.signal['4g']) return;
-    const _4gStatsEl = document.getElementById('4g-stats');
-    _4gStatsEl!.innerHTML = ''; // eslint-disable-line
-    for (const key in this._stats.signal['4g']) {
-      const item = document.createElement('div');
-      const label = document.createElement('span');
-      label.classList.add('stat-label');
-      label.innerText = key;
-      const stat = document.createElement('span');
-      stat.innerText = this._stats.signal['4g'][key];
-      item.appendChild(label);
-      item.append(': ');
-      item.appendChild(stat);
-      _4gStatsEl!.appendChild(item); // eslint-disable-line
+  updateSignals() {
+    for (const signal in this._stats.signal) {
+      if (this['_' + signal + 'Stats'] == this._stats.signal[signal]) return;
+      const el = document.getElementById(signal + '-stats');
+      for (const key in this._stats.signal[signal]) {
+        const item = document.createElement('div');
+        const label = document.createElement('span');
+        label.classList.add('stat-label');
+        label.innerText = key;
+        const stat = document.createElement('span');
+        stat.innerText = this._stats.signal[signal][key];
+        item.appendChild(label);
+        item.append(': ');
+        item.appendChild(stat);
+        el!.appendChild(item); // eslint-disable-line
+      }
+      this['_' + signal + 'Stats'] = this._stats.signal['4g'];
     }
-    this._4gStats = this._stats.signal['4g'];
-  }
-
-  update5g() {
-    if (this._5gStats == this._stats.signal['5g']) return;
-    // Convention to start var with a number?
-    const _5gStatsEl = document.getElementById('5g-stats');
-    _5gStatsEl!.innerHTML = ''; // eslint-disable-line
-    for (const key in this._stats.signal['5g']) {
-      const item = document.createElement('div');
-      const label = document.createElement('span');
-      label.classList.add('stat-label');
-      label.innerText = key;
-      const stat = document.createElement('span');
-      stat.innerText = this._stats.signal['5g'][key];
-      item.appendChild(label);
-      item.append(': ');
-      item.appendChild(stat);
-      _5gStatsEl!.appendChild(item); // eslint-disable-line
-    }
-    this._5gStats = this._stats.signal['5g'];
-
   }
 
   async getConfiguration() {
